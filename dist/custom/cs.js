3cac07b84687978769d618f93b487a0bbc20c0a0
anjanadevelop <83876898+anjanadevelop@users.noreply.github.com>
2022-06-18 12:36:29 +0530

    first commit
    changes

---
+++ dist/custom/cs.js
@@ -0,0 +1,10 @@
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+      // document ready
+
+    //   alert("hi");
+
+    document.getElementById('overfull').style.display = 'none';
+
+    }
+  };
